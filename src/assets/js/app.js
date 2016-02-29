a2a713598e497be45dd16884e1035d7c44a53557
patrickgrey <websemantic@yahoo.co.uk>
2016-02-29 16:19:01 +0000

    Adds sticky nav bar to top

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,6 @@
 $(document).foundation();
+  $('.title-bar').on('sticky.zf.stuckto:top', function(){
+  $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function(){
+  $(this).removeClass('shrink');
+});
