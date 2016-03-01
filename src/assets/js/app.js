3b98c31dfc62fd9324fac13ec5edb90e5c68de0b
patrickgrey <websemantic@yahoo.co.uk>
2016-03-01 17:12:21 +0000

    Adds basic content and sticky menu, yet to test on actual mobile

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation();
+//offset() measures how far down the sticky element is from the top of the window
+var stickyTop = $(".sticky").offset().top;
+
+//whenever the user scrolls, measure how far we have scrolled
+//
+//NEED TO DEBOUNCE THIS!!!
+$(window).scroll(function() {
+  var windowTop = $(window).scrollTop();
+
+  //check to see if we have scrolled past the original location of the sticky element
+  if (windowTop > stickyTop) {
+
+    //if so, change the sticky element to fised positioning
+    $(".sticky").addClass("stuck");
+  } else {
+    $(".sticky").removeClass("stuck");
+  }
+});
